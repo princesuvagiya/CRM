35ae6dec6e3deff842f54267d36da6cda7db4bc3
princesuvagiya <prince.suhagiya@shunyavkash.com>
2022-08-19 17:57:31 +0530

    Category page WIP ?->admin.category

---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,32 @@
+@extends('admin.layouts.master')
+
+@section('content')
+
+@include('admin.components.header')
+
+@if(session('msg'))
+<div class="alert alert-success">
+    {{session('msg') }}
+</div>
+@endif
+
+<div class="container-fluid pt-4 px-4 ">
+    <div class="row g-4">
+        <div class="col-sm-12 col-xl-6 ">
+            <div class="bg-light rounded h-100 p-4">
+                <h6 class="mb-4">company</h6>
+                <form action="{{ url('/InsertData') }}" method="POST" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                            Enter Category
+                        </label>
+                        <input type="text" name="category" class="form-control" id="exampleInputEmail1" placeholder="Enter Category">
+                    </div>
+                    <input type="submit" name="submit" value="Sing in" class="btn btn-primary">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
